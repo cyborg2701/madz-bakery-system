42199d4e2bf5d2981dbb31bf977b185a291e3216
cyborg2701 <phc-cyborg@nemsu.edu.ph>
2022-08-24 20:08:23 +0800

    add online database

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('mysql://bae2951b49e7ff:205b61db@us-cdbr-east-06.cleardb.net/heroku_9ef379c60fc4c33?reconnect=true'),
+            'host' => env('DB_HOST', 'us-cdbr-east-06.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_9ef379c60fc4c33'),
+            'username' => env('DB_USERNAME', 'bae2951b49e7ff'),
+            'password' => env('DB_PASSWORD', '205b61db'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
